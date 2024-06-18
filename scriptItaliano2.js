167d4c6d1d4dc0e7c2cd964e96d6839e59883777
sebastian <c.tanase@studenti.uniba.it>
2024-06-18 22:41:37 +0200

    fix litonea e culturale scrpt

--- scriptItaliano2.js
+++ scriptItaliano2.js
@@ -76,79 +76,12 @@ const contentString = `<div id="content">
   <figure>
   <img src="../img/cover.jpg"">
   <figcaption>Listen to the T-Rex:</figcaption>
-  <audio controls src="../media/audio.mp3">
-  </audio>
+<!--  <audio controls src="../media/audio.mp3">
+  </audio>-->
   </figure>
   </div>`
 
 
-const templatePopup = `
-  <div id="audio-player-container">
-  <audio src="https://assets.codepen.io/4358584/Anitek_-_Komorebi.mp3" preload="metadata" loop></audio>
-  <p>audio player ish</p>
-  <button id="play-icon"></button>
-  <span id="current-time" class="time">0:00</span>
-  <input type="range" id="seek-slider" max="100" value="0">
-  <span id="duration" class="time">0:00</span>
-  <output id="volume-output">100</output>
-  <input type="range" id="volume-slider" max="100" value="100">
-  <button id="mute-icon"></button>
-</div>`;
-
-
-
-const templatePupup2 = `
-   <div class="player">
-       <div class="wrapper">
-           <div class="details">
-               <div class="now-playing">PLAYING x OF y</div>
-               <div class="track-art"></div>
-               <div class="track-name">Track Name</div>
-               <div class="track-artist">Track Artist</div>
-           </div>
-
-           <div class="slider_container">
-               <div class="current-time">00:00</div>
-                <input type="range" min="1" max="100" value="0" class="seek_slider" onchange="seekTo()">
-                <div class="total-duration">00:00</div>
-           </div>
-
-
-
-             <div class="playpause-track" onclick="playpauseTrack()">
-                    <i class="fa fa-play-circle fa-5x"></i>
-            </div>
-            <!--
-           <div class="buttons">
-               <div class="random-track" onclick="randomTrack()">
-                   <i class="fas fa-random fa-2x" title="random"></i>
-               </div>
-               <div class="prev-track" onclick="prevTrack()">
-                    <i class="fa fa-step-backward fa-2x"></i>
-                </div>
-                <div class="playpause-track" onclick="playpauseTrack()">
-                    <i class="fa fa-play-circle fa-5x"></i>
-                </div>
-                <div class="next-track" onclick="nextTrack()">
-                    <i class="fa fa-step-forward fa-2x"></i>
-                </div>
-                <div class="repeat-track" onclick="repeatTrack()">
-                    <i class="fa fa-repeat fa-2x" title="repeat"></i>
-                </div>
-           </div>
-           -->
-
-            <div id="wave">
-                <span class="stroke"></span>
-                <span class="stroke"></span>
-                <span class="stroke"></span>
-                <span class="stroke"></span>
-                <span class="stroke"></span>
-                <span class="stroke"></span>
-                <span class="stroke"></span>
-            </div>
-       </div>
-   </div>`;
 
 async function initMap() {
   const myLatLng = { lat: 40.473187425126454, lng: 17.23316609859467 };
@@ -201,7 +134,7 @@ async function initMap() {
 
 
   const icon = {
-    url: "./img/0.png", // url
+    url: "STATIC/assets/img/bisceglie/img/0.png", // url
     scaledSize: new google.maps.Size(size, size), // scaled size
     size: new google.maps.Size(size,size), //size
     //origin: null, //origin
@@ -241,7 +174,7 @@ async function initMap() {
   // Create the markers.
   tourStops.forEach(([position, title], i) => {
     let icon = {
-      url: "./img/"+(i+1)+".png", // url
+      url: "STATIC/assets/img/bisceglie/img/"+(i+1)+".png", // url
       scaledSize: new google.maps.Size(size, size), // scaled size
       size: new google.maps.Size(size,size), //size
     };
@@ -313,6 +246,6 @@ async function initMap() {
 }
 
 
-initMap();
+await initMap();
 
 
